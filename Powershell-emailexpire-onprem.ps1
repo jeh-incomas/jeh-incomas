045d1e587127a03d6270aebb97749ddd8f7dc302
jeh-incomas <jeh@in-commodities.com>
2023-10-11 13:35:35 +0200

    Change the loop through each user account to be able to have it in emailbody easier

--- Powershell-emailexpire-onprem.ps1
+++ Powershell-emailexpire-onprem.ps1
@@ -1,17 +1,15 @@
 #Import AD Module
 Import-Module ActiveDirectory
- 
-#Create warning dates for future password expiration
-$SevenDayWarnDate = (get-date).adddays(7).ToLongDateString()
 
-#Email Variables
-$Credential="sias@in-commodities.com"
-$SMTPServer = 'smtp.office365.com'
-$MailSender = "Password AutoBot <sias@incommodities.com>"
-$Subject = 'INCOM - Your account password will expire soon'
-$EmailBody = "
-    Hello $($user),
-    Your password expires $remainingDays.
+#Define the notification time in days
+$DaysToExpire = '7'
+
+#Define the email subject, body, and sender address
+$SmtpServer="smtp.office365.com"
+$EmailFrom = "sias@incommodities.com"
+$EmailSubject = 'INCOM - Your account password will expire soon'
+$EmailBody = "Hello {0},
+    Your password expires in $DaysLeft day(s)
     It is important that you change your password ASAP to avoid any disruption.      
     How do I change my password.
         1. If you are not at the Office connect to the VPN(Cisco AnyConnect).
@@ -21,19 +19,35 @@ $EmailBody = "
         5. Unlock your computer with your new password            
     If you have any questions please contact us via support@in-commodities.com or call us at +45 8180 8805"
 
-#Find accounts that are enabled and have expiring passwords
-$users = Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False -and PasswordLastSet -gt 0 } `
- -Properties "Name", "EmailAddress", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Name", "EmailAddress", `
- @{Name = "PasswordExpiry"; Expression = {[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed").tolongdatestring() }}
- 
-#Script for giving the user how much time is left
-$remainingDays="Get-ADuser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False -and PasswordLastSet -gt 0"
+# Assign a fixed value to the maximum password age in days
+$MaxPasswordAge = New-TimeSpan -Days "180" # or (Get-Date "2024-04-09")
 
-#check password expiration date and send email on match
-foreach ($user in $users) {
-     if ($user.PasswordExpiry -eq $SevenDayWarnDate) {
-         $days = 7
-         Send-MailMessage -To "jeh@in-commodities.com" -From $MailSender -SmtpServer $SMTPServer -Subject $Subject -Body $EmailBody -UseSsl -Port 587 -Credential $Credential
-     }
-    else {}
- }
+# Get the user accounts and their password expiration dates, excluding those with password neverexpires
+$Users = Get-ADUser -Filter {PasswordNeverExpires -eq $false} -Properties PasswordLastSet, PasswordNeverExpires, PasswordExpired, DisplayName
+
+# Loop through each user account
+foreach ($User in $Users) {
+
+    # Skip the user if the password is already expired
+    if ($User.PasswordExpired -eq $true) {
+        continue
+    }
+
+    # Calculate the password expiration date and the remaining days
+    if ($User.PasswordLastSet -ne $null) {
+        $PasswordExpiryDate = $User.PasswordLastSet.Add($MaxPasswordAge)
+        $DaysLeft = (New-TimeSpan -Start (Get-Date) -End $PasswordExpiryDate).Days
+    }
+
+    # Check if the remaining days are less than or equal to the notification time
+    if ($DaysLeft -le $DaysToExpire) {
+
+        # Get the user's email address
+        $EmailTo = $User.EmailAddress
+
+        # Format the email body with the user's name, display name, and the remaining days
+        $EmailBodyFormatted = $EmailBody -f $User.Name, $User.DisplayName, $DaysLeft
+        #Send an email to the user
+        Send-MailMessage -To "jeh@in-commodities.com" -From $EmailFrom -Subject $EmailSubject -Body $EmailBodyFormatted -SmtpServer $SmtpServer  -UseSsl -Port 587 -Credential $Credential
+    }
+}
